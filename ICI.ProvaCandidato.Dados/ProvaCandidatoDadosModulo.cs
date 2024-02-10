1d408706b0749088a42458351e6285c0d9bf28d0
Marcio José Ferreira dos Santos <marcioj.fsantos@outlook.com>
2024-02-10 15:13:25 -0300

    Criado camada de Repositorio, serviço e listagem inicial de TAGS

---
+++ ICI.ProvaCandidato.Dados/ProvaCandidatoDadosModulo.cs
@@ -0,0 +1,25 @@
+﻿using ICI.ProvaCandidato.Dados.Contextos;
+using ICI.ProvaCandidato.Dados.Repositorios;
+using ICI.ProvaCandidato.Negocio.Contratos.Repositorios;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ICI.ProvaCandidato.Dados
+{
+    public static class ProvaCandidatoDadosModulo
+    {
+        public static IServiceCollection AddModuloDados(this IServiceCollection services, IConfiguration Configuration) 
+        {
+            services.AddDbContext<AppDbContexto>(options =>
+            {
+                string connection = Configuration.GetConnectionString("Default");
+                options.UseSqlServer(connection);
+            });
+
+            services.AddScoped<ITagRepositorio, TagRepositorio>();
+
+            return services;
+        }
+    }
+}
---
+++ ICI.ProvaCandidato.Dados/Repositorios/TagRepositorio.cs
@@ -0,0 +1,26 @@
+﻿using ICI.ProvaCandidato.Dados.Contextos;
+using ICI.ProvaCandidato.Negocio.Contratos.Repositorios;
+using ICI.ProvaCandidato.Negocio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICI.ProvaCandidato.Dados.Repositorios
+{
+    public class TagRepositorio : ITagRepositorio
+    {
+        private readonly AppDbContexto _contexto;
+        public TagRepositorio(AppDbContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IEnumerable<Tag>> ObterTagsAsync()
+        {
+            return await this._contexto.Tags.ToListAsync();
+        }
+    }
+}
