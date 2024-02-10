0f8c815451519a18cac5b386d382699dbc737a94
Marcio José Ferreira dos Santos <marcioj.fsantos@outlook.com>
2024-02-10 19:01:35 -0300

    Criado o CRUD de tags

--- ICI.ProvaCandidato.Dados/Repositorios/TagRepositorio.cs
+++ ICI.ProvaCandidato.Dados/Repositorios/TagRepositorio.cs
@@ -2,10 +2,7 @@
 using ICI.ProvaCandidato.Negocio.Contratos.Repositorios;
 using ICI.ProvaCandidato.Negocio.Entidades;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ICI.ProvaCandidato.Dados.Repositorios
@@ -18,9 +15,35 @@ namespace ICI.ProvaCandidato.Dados.Repositorios
             _contexto = contexto;
         }
 
+        public async Task AdicionarAsync(Tag tag)
+        {
+            this._contexto.Tags.Add(tag);
+            await this._contexto.SaveChangesAsync();
+        }
+
+        public async Task AtualizarAsync(Tag tag)
+        {
+            this._contexto.Entry(tag).State = EntityState.Modified;
+            await this._contexto.SaveChangesAsync();
+        }
+
+        public async Task ExcluirAsync(Tag tag)
+        {
+            this._contexto.Tags.Remove(tag);
+            await this._contexto.SaveChangesAsync();
+        }
+
+        public async Task<Tag> ObterPorIdAsync(int id)
+        {
+            return await this._contexto.Tags.Include(tag => tag.NoticiaTags).FirstOrDefaultAsync(tag => tag.Id == id);
+
+        }
+
         public async Task<IEnumerable<Tag>> ObterTagsAsync()
         {
-            return await this._contexto.Tags.ToListAsync();
+            return await this._contexto.Tags
+                .Include(tag => tag.NoticiaTags)
+                .ToListAsync();
         }
     }
 }
