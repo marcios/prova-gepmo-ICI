7d7b30d043c0aedf71a81a9583fd59646d1ee01f
Marcio José Ferreira dos Santos <marcioj.fsantos@outlook.com>
2024-02-10 13:30:07 -0300

    criado a migracao da entidade tag

---
+++ ICI.ProvaCandidato.Dados/Migrations/20240210162401_Add_Tag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ICI.ProvaCandidato.Dados.Migrations
+{
+    public partial class Add_Tag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tag");
+        }
+    }
+}
